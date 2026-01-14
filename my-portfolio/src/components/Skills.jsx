0cbf293e114a44ab83e11a553dfc0e5848dbe92c
farguello <franco.arguello@siliconmisiones.gob.ar>
2026-01-14 18:39:58 -0300

    Projects and skills updated

--- my-portfolio/src/components/Skills.jsx
+++ my-portfolio/src/components/Skills.jsx
@@ -11,21 +11,29 @@ const Skills = () => {
         <h3 className="text-3xl font-bold mb-4 text-white">{t("skillsTitle")}</h3>
         <div className='mt-4 flex flex-wrap gap-3 justify-between md:justify-start'>
           <SkillPill skill="Agile" level={40} />
-          <SkillPill skill="Bootstrap" level={20} />
-          <SkillPill skill="CSS" level={50} />
-          <SkillPill skill="Docker" level={20} />
-          <SkillPill skill="Figma" level={40} />
-          <SkillPill skill="Git" level={50} />
+          <SkillPill skill="Bootstrap" level={30} />
+          <SkillPill skill="CSS" level={60} />
+          <SkillPill skill="Docker" level={40} />
+          <SkillPill skill="Figma" level={30} />
           <SkillPill skill="GitHub" level={30} />
-          <SkillPill skill="JavaScript" level={40} />
-          <SkillPill skill="Jira" level={50} />
-          <SkillPill skill="MySQL" level={30} />
-          <SkillPill skill="Node.js" level={30} />
-          <SkillPill skill="PostgreSQL" level={40} />
+          <SkillPill skill="JavaScript" level={50} />
+          <SkillPill skill="Jira" level={80} />
+          <SkillPill skill="MySQL" level={40} />
+          <SkillPill skill="Node.js" level={40} />
+          <SkillPill skill="PostgreSQL" level={30} />
           <SkillPill skill="React" level={80} />
-          <SkillPill skill="SCRUM" level={30} />
-          <SkillPill skill="SQL" level={40} />
-          <SkillPill skill="Tailwind" level={60} />
+          <SkillPill skill="SCRUM" level={50} />
+          <SkillPill skill="SQL" level={60} />
+          <SkillPill skill="Tailwind" level={80} />
+          <SkillPill skill="NextJS" level={40} />
+          <SkillPill skill="Prisma" level={30} />
+          <SkillPill skill="Gitlab" level={60} />
+          <SkillPill skill="Typescript" level={30} />
+          <SkillPill skill="Git" level={50} />
+
+          <SkillPill skill="CI/CD" level={20} />
+
+
 
         </div>
       </div>
